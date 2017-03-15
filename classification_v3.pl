63f629aaae2c0fa844aae022f5123c7c1a223d76
Alejandro A. Schaffer <schaffer@cbbdev12.be-md.ncbi.nlm.nih.gov>
2017-03-15 09:50:58 -0400

    initial setup of repositiory from /panfs/pan1.be-md.ncbi.nlm.nih.gov/dnaorg/2015.09/16S/round9

---
+++ classification_v3.pl
@@ -0,0 +1,149 @@
+#!/usr/bin/perl -w
+# the first line of perl code has to be above
+#
+# Author: Alejandro Schaffer
+# Code to classify whether a sequence is a 16S RNA or not.
+# possible classifications are: yes, no, partial, too long, or too short
+# the separation of the too long an too short classes is done in an earlier
+# program and passed through this program
+# Usage: classification_v3.pl <length_summary file> <BLAST results file in tab-delimited format 6> <output> <identity threshold> <E-value threshold>
+
+use strict;
+use warnings;
+
+my $length_input_file; #input file with query name, length, and preliminary classification
+my $BLAST_input_file; #input file with BLAST results
+my $output_file; #output file;
+my @query_names; #array of tokens that are treated as the names of the queries; query names are
+                 #assumed to be unique in the set
+my @query_classifications; #array of query classifications, partly based on preprocessing for length
+my @query_strand_match; #array of + or - indicating whether best match, if any is on the same strand 
+my @query_num_alignments_best_match; #array indicating the number of local alignments to the best match  (> 1 is anomalous)
+my @query_lengths; #array of query lengths;
+my %query_indices; #hash mapping names to indices;
+my $num_queries;
+my $query_index;
+my $nextline;
+my @fields;
+my $this_index;
+
+my $match_E_threshold;
+
+
+my $QUERY_COLUMN = 0;
+my $E_COLUMN = 10;
+my $IDENTITY_COLUMN = 2;
+my $QLENGTH_COLUMN = 3;
+my $SUBJECT_START_COLUMN = 8;
+my $SUBJECT_END_COLUMN = 9;
+
+my $TOO_LONG = -2;
+my $TOO_SHORT = -1;
+my $NO_MATCH = 0;
+my $FULL_MATCH = 1;
+my $PARTIAL = 2;
+my $IMPERFECT = 3;
+
+my $lower_length = 900;
+my $upper_length = 1800;
+my $identity_threshold;
+
+my @output_strings;
+$output_strings[0] = "too long\tNA\tNA";
+$output_strings[1] = "too short\tNA\tNA";
+$output_strings[2] = "no\tNA\tNA";
+$output_strings[3] = "yes\t";
+$output_strings[4] = "partial\t";
+$output_strings[5] = "imperfect_match\t";
+
+my $usage = "classification_v2.pl <length_summary file> <BLAST results file in tab-delimited format 6> <output> <identity threshold> <Evalue threshold";
+
+if (@ARGV != 5) {
+	print "The correct number of arguments is 5\n";
+	die $usage;
+}
+
+$length_input_file = $ARGV[0];
+$BLAST_input_file = $ARGV[1];
+$output_file = $ARGV[2];
+$identity_threshold = $ARGV[3];
+$match_E_threshold = $ARGV[4];
+
+open(LENGTH_INPUT, "<$length_input_file") or die "Cannot open 1 $length_input_file\n"; 
+
+$query_index  = 0;
+while(defined($nextline = <LENGTH_INPUT>)) {
+    chomp($nextline);
+    @fields = split /\t/, $nextline;
+    $query_names[$query_index] = $fields[0];
+    $query_classifications[$query_index] = $fields[1];
+    $query_lengths[$query_index] = $fields[2]; 
+    $query_indices{$fields[0]} = $query_index;
+    $query_index++;
+}
+$num_queries = $query_index;
+
+close(LENGTH_INPUT); 
+
+open(BLAST_INPUT, "<$BLAST_input_file") or die "Cannot open 2 $BLAST_input_file\n"; 
+open(OUTPUT, ">$output_file") or die "Cannot open 3 $output_file\n";
+
+
+
+
+
+while(defined($nextline = <BLAST_INPUT>)) {
+    chomp($nextline);
+    @fields = split /\t/, $nextline;
+    if (defined($query_indices{$fields[$QUERY_COLUMN]})) {
+	$this_index = $query_indices{$fields[$QUERY_COLUMN]};
+	$query_num_alignments_best_match[$this_index]++; 
+	if ((($fields[$E_COLUMN] == 0.0) ||
+	     ($fields[$E_COLUMN] < $match_E_threshold)) &&
+	    ($fields[$IDENTITY_COLUMN] > $identity_threshold)) {
+	    $query_classifications[$this_index] = $FULL_MATCH;
+	    if (($fields[$QLENGTH_COLUMN] < $lower_length) ||
+		($fields[$QLENGTH_COLUMN] > $upper_length)) {
+		$query_classifications[$this_index] = $PARTIAL;
+	    }
+	}
+	else {
+	    if (!($query_classifications[$this_index] eq $FULL_MATCH) &&
+		!($query_classifications[$this_index] eq $PARTIAL)) {
+		$query_classifications[$this_index] = $IMPERFECT;
+	    }
+	}
+	if ($fields[$SUBJECT_START_COLUMN] < $fields[$SUBJECT_END_COLUMN]) {
+	    if ((!defined($query_strand_match[$this_index])) || ("plus" eq $query_strand_match[$this_index])) {
+		$query_strand_match[$this_index] = "plus";
+	    }
+	    else {
+		$query_strand_match[$this_index] = "mixed";
+	    }
+	}
+	else {
+	    if ((!defined($query_strand_match[$this_index])) || ("minus" eq $query_strand_match[$this_index])) {
+		$query_strand_match[$this_index] = "minus";
+	    }
+	    else {
+		$query_strand_match[$this_index] = "mixed";
+	    }
+	}
+    }
+    else {
+	printf "Cannot find index for query $fields[0]\n";
+    }
+
+}
+
+for($query_index = 0; $query_index < $num_queries; $query_index++) {
+    print OUTPUT "$query_names[$query_index]";
+    print OUTPUT "\t";
+    print OUTPUT $output_strings[$query_classifications[$query_index]+2];
+    if ($query_classifications[$query_index] > 0) {
+	print OUTPUT "$query_strand_match[$query_index]\t$query_num_alignments_best_match[$query_index]";
+    }
+    print OUTPUT "\n";
+}
+
+close(OUTPUT);
---
+++ partition_by_length_v2.pl
@@ -0,0 +1,115 @@
+#!/usr/bin/perl -w
+# the first line of perl code has to be above
+#
+# Author: Alejandro Schaffer
+# Code to partition a fasta file into two fasta files
+# depending on whether the length of the sequence is within a
+# prescribed range or not
+# Usage: partition_by_length.pl <input fasta file>  <output file in range> <output file out of range> <output_summary> <lower bound> <upper bound>
+
+use strict;
+use warnings;
+
+my $input_file; #input fasta file 
+my $middle_output_file; #output fasta file for sequences in the middle range
+my $extreme_output_file; #output fasta file for sequences outside the middle range
+my $output_summary_file; #output file with classifications
+my $nextline; #one line of the file
+my $total_length; #total length of a sequence
+my $this_length; #length of one sequence
+my $lower_bound; #lower bound on lengths in the middle range
+my $upper_bound; #upper bound on lengths in the middle range
+my $query_name; #identifier for one query
+ 
+
+my @length_array; #array of sequence lengths
+my $sequence_index; #index on sequences;
+my $in_range; #is the length of the sequence in the prescribed range
+
+my $TOO_LONG = -2;
+my $TOO_SHORT = -1;
+my $NO_MATCH = 0;
+my $FULL_MATCH = 1;
+my $PARTIAL = 2;
+
+my $usage = "partition_by_length.pl <input fasta file>  <output file in range> <output file out of range> <output_summary><lower bound> <upper bound>\n";
+
+$input_file = $ARGV[0];
+$middle_output_file = $ARGV[1];
+$extreme_output_file = $ARGV[2];
+$output_summary_file = $ARGV[3];
+$lower_bound = $ARGV[4];
+$upper_bound = $ARGV[5];
+
+
+if (@ARGV != 6) {
+    print "The correct number of arguments is 6\n";
+    die $usage;
+}
+
+open(INPUT, "<$input_file") or die "Cannot open 1 $input_file\n"; 
+open(MIDDLE_OUTPUT, ">$middle_output_file") or die "Cannot open 2 $middle_output_file\n"; 
+open(EXTREME_OUTPUT, ">$extreme_output_file") or die "Cannot open 3 $extreme_output_file\n"; 
+open(OUTPUT_SUMMARY, ">$output_summary_file") or die "Cannot open 4 $output_summary_file\n"; 
+
+$sequence_index  = 0;
+$total_length = 0;
+while(defined($nextline = <INPUT>)) {
+    chomp($nextline);
+    if (($nextline =~m/^>/)) {
+	if ($total_length > 0) {
+	    $length_array[$sequence_index] = $total_length;
+	    $sequence_index++;
+	}
+	$total_length = 0;
+    }
+    else {
+	$this_length = length($nextline);
+	# print "Adding $this_length to $total_length\n";
+	$total_length += $this_length;
+    }
+}
+$length_array[$sequence_index] = $total_length;
+close(INPUT);
+
+$sequence_index  = 0;
+$in_range = 1;
+open(INPUT, "<$input_file") or die "Cannot open 4 $input_file\n"; 
+while(defined($nextline = <INPUT>)) {
+    chomp($nextline);
+    if (($nextline =~m/^>/)) {
+        ($query_name) = ($nextline =~m/^>(\S+)/);
+	if (($length_array[$sequence_index] >= $lower_bound) &&
+            ($length_array[$sequence_index] <= $upper_bound)) {
+	    $in_range = 1;
+	    print OUTPUT_SUMMARY "$query_name\t0\t";
+	    print OUTPUT_SUMMARY "$length_array[$sequence_index]";
+	    print OUTPUT_SUMMARY "\n";
+	}
+	else {
+	    if ($length_array[$sequence_index] < $lower_bound) {
+		$in_range = $TOO_SHORT;
+	    }
+	    else {
+		$in_range = $TOO_LONG;
+	    }
+	    print OUTPUT_SUMMARY "$query_name\t";
+	    print OUTPUT_SUMMARY "$in_range";
+	    print OUTPUT_SUMMARY "\t$length_array[$sequence_index]";
+	    print OUTPUT_SUMMARY "\n";
+	}
+	$sequence_index++;
+    }
+    if (1 == $in_range) {
+	print MIDDLE_OUTPUT $nextline;
+	print MIDDLE_OUTPUT "\n";
+    }
+    else {
+	print EXTREME_OUTPUT $nextline;
+	print EXTREME_OUTPUT "\n";
+    }
+}
+close (MIDDLE_OUTPUT);
+close (EXTREME_OUTPUT);
+close (OUTPUT_SUMMARY);
+close(INPUT);
